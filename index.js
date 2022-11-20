6da1f01dd334d4a36284ddd3df5e1154f8107638
Kenny Johnson <105308787+papercircuit@users.noreply.github.com>
2022-11-20 15:28:39 -0500

    Firing is working

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ let render = Matter.Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: 1600,
-        height: 900,
+        width: 1200,
+        height: 800,
         wireframes: false
     }
 });
 
-let ground = Matter.Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+let ground = Matter.Bodies.rectangle(900, 500, 300, 20, { isStatic: true });
 let boxA = Matter.Bodies.rectangle(400, 200, 80, 80);
 let boxB = Matter.Bodies.rectangle(450, 50, 80, 80);
 
-const mouse = Matter.Mouse.create(render.canvas);
-const mouseConstraint = Matter.MouseConstraint.create(engine, {
+let mouse = Matter.Mouse.create(render.canvas);
+let mouseConstraint = Matter.MouseConstraint.create(engine, {
     mouse: mouse,
     constraint: {
         render: { visible: false }
@@ -23,15 +23,34 @@ const mouseConstraint = Matter.MouseConstraint.create(engine, {
 });
 render.mouse = mouse;
 
-// the first two arguments to stack are the x and y position of the stack
-// the next two are the number of objects in the stack and the spacing between them
-const stack = Matter.Composites.stack(20, 20, 10, 5, 0, 0, function(x, y) {
-    const sides = Math.round(Matter.Common.random(2, 8));
-    return Matter.Bodies.polygon(x, y, sides, Matter.Common.random(20, 50));
+let ball = Matter.Bodies.circle(300, 600, 20);
+let sling = Matter.Constraint.create({
+    pointA: { x: 300, y: 600 },
+    bodyB: ball,
+    stiffness: 0.05
+});
+
+let stack = Matter.Composites.stack(800, 270, 4, 4, 10, 0, function (x, y) {
+    return Matter.Bodies.polygon(x, y, 8, 30);
+});
+
+let firing = false;
+Matter.Events.on(mouseConstraint, "enddrag", function (event) {
+    if (event.body === ball) firing = true;
+});
+
+Matter.Events.on(engine, 'afterUpdate', function () {
+    if (firing && Math.abs(ball.position.x - 300) < 20 && Math.abs(ball.position.y - 600) < 30) {
+        ball = Matter.Bodies.circle(300, 600, 20);
+        Matter.World.add(engine.world, ball);
+        sling.bodyB = ball;
+        firing = false;
+    }
 });
 
 
 
-Matter.World.add(engine.world, [stack, ground, mouseConstraint]);
+
+Matter.World.add(engine.world, [stack, ground, ball, sling, mouseConstraint]);
 Matter.Runner.run(engine);
 Matter.Render.run(render);
