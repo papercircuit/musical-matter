7d5004fb0783be2b2787b818ebc0b99410ebd954
Kenny Johnson <105308787+papercircuit@users.noreply.github.com>
2022-11-21 08:49:05 -0500

    Mostly working

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ let render = Matter.Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: 1200,
-        height: 800,
+        width: 800,
+        height: 1200,
         wireframes: false,
         // background: 'url(./windchime.png)',
         // backgroundSize: 'cover',
@@ -15,16 +15,26 @@ let render = Matter.Render.create({
         showVelocity: true,
         showPositions: true,
         showConvexHulls: true,
-        showAxes: true,
+        showAxes: false,
     }
 });
 
 // place ground at bottom of screen and set to screen width
-let ground = Matter.Bodies.rectangle(600, 800, 1200, 50, { isStatic: true });
+let ground = Matter.Bodies.rectangle(400, 1200, 810, 100, { isStatic: true });
 
 // place walls at sides of screen from ground to 50% screen height
-let leftWall = Matter.Bodies.rectangle(0, 400, 50, 800, { isStatic: true });
-let rightWall = Matter.Bodies.rectangle(1200, 400, 50, 800, { isStatic: true });
+let leftWall = Matter.Bodies.rectangle(0, 600, 60, 1200, { isStatic: true });
+let rightWall = Matter.Bodies.rectangle(800, 600, 60, 1200, { isStatic: true });
+
+// create angular "walls" to keep ball in play
+let leftAngularWall1 = Matter.Bodies.rectangle(340, 200, 10, 200, { isStatic: true, angle: 2 });
+
+let rightAngularWall1 = Matter.Bodies.rectangle(500, 400, 10, 200, { isStatic: true, angle: -2 });
+
+let leftAngularWall2 = Matter.Bodies.rectangle(300, 600, 10, 200, { isStatic: true, angle: 2 });
+
+let rightAngularWall2 = Matter.Bodies.rectangle(500, 800, 10, 200, { isStatic: true, angle: -2 });
+
 
 let mouse = Matter.Mouse.create(render.canvas);
 let mouseConstraint = Matter.MouseConstraint.create(engine, {
@@ -38,45 +48,70 @@ render.mouse = mouse;
 
 // RANDOM BODIES
 
-let stack = Matter.Composites.stack(800, 270, 20, 20, 0, 0, function (x, y) {
+let stack = Matter.Composites.stack(200, 100, 5, 5, 0, 0, function (x, y) {
     // create random body types and sizes 
-    let sides = Math.round(Matter.Common.random(4, 6));
+    let sides = Math.round(Matter.Common.random(4, 9));
     let options = {
-        friction: 0.001,
+        friction: 1,
         restitution: 0.1,
-        density: 100,
-        mass: 100,
+        density: 1,
+        mass: 10,
     };
 
     switch (Math.round(Matter.Common.random(0, 1))) {
         case 0:
             if (Matter.Common.random() < 0.8) {
-                return Matter.Bodies.rectangle(x, y, Matter.Common.random(10, 100), Matter.Common.random(25, 50), options);
+                return Matter.Bodies.rectangle(x, y, Matter.Common.random(3, 34), Matter.Common.random(25, 50), options);
             }
             else {
-                return Matter.Bodies.rectangle(x, y, Matter.Common.random(80, 120), Matter.Common.random(25, 30), options);
+                return Matter.Bodies.rectangle(x, y, Matter.Common.random(3, 45), Matter.Common.random(25, 30), options);
             }
         case 1:
-            return Matter.Bodies.polygon(x, y, sides, Matter.Common.random(25, 50), options);
+            return Matter.Bodies.polygon(x, y, sides, Matter.Common.random(5, 25), options);
     }
 
 });
 
+// Every second, drop a random new object from the top middle of the screen
+setInterval(function () {
+    let sides = Math.round(Matter.Common.random(4, 9));
+    let options = {
+        friction: 1,
+        restitution: 0.1,
+        density: 1,
+        mass: 10,
+    };
+
+    switch (Math.round(Matter.Common.random(0, 1))) {
+        case 0:
+            if (Matter.Common.random() < 0.8) {
+                return Matter.World.add(engine.world, Matter.Bodies.rectangle(400, 0, Matter.Common.random(3, 34), Matter.Common.random(25, 50), options));
+            }
+            else {
+                return Matter.World.add(engine.world, Matter.Bodies.rectangle(400, 0, Matter.Common.random(3, 45), Matter.Common.random(25, 30), options));
+            }
+        case 1:
+            return Matter.World.add(engine.world, Matter.Bodies.polygon(400, 0, sides, Matter.Common.random(5, 25), options));
+    }
+}, 1000);
+
+
+
 // BALL AND SLING
 
 // place ball in top center of canvas
-let ball = Matter.Bodies.circle(600, 50, 25, { restitution: 0.8, lable: 'slingball' });
-let sling = Matter.Constraint.create({
-    pointA: { x: 500, y: 0 },
-    bodyB: ball,
-    stiffness: 0.005,
-    length: 100,
-    render: {
-        strokeStyle: '#ffffff',
-        lineWidth: 5
-    },
-    label: 'sling'
-});
+// let ball = Matter.Bodies.circle(600, 50, 25, { restitution: 0.8, lable: 'slingball' });
+// let sling = Matter.Constraint.create({
+//     pointA: { x: 500, y: 0 },
+//     bodyB: ball,
+//     stiffness: 0.005,
+//     length: 100,
+//     render: {
+//         strokeStyle: '#ffffff',
+//         lineWidth: 5
+//     },
+//     label: 'sling'
+// });
 
 
 
@@ -100,41 +135,59 @@ let fire = function (event) {
 const randomRadius = () => {
     return Math.floor(Math.random() * 50) + 10;
 }
-;
+    ;
 
 Matter.Events.on(engine, 'collisionStart', function (event) {
     let body = event.pairs[0].bodyA;
     let velocity = body.velocity;
-    let volume = Math.min(Math.abs(velocity.x * .05 ) + Math.abs(velocity.y * .05), 1);
+    let volume = Math.min(Math.abs(velocity.x * .05) + Math.abs(velocity.y * .05), 1);
 
-   // quantize pitch to C major pentatonic scale
+    // quantize pitch to C major pentatonic scale
     let pitch = Math.round(body.position.x / 100) * 100;
     let note = Math.round(pitch / 100) % 5;
     let notes = [261.63, 329.63, 392, 523.25, 659.25];
     let notePitch = notes[note];
+    let noteDuration = 0.1;
+
+    // const reverb = new Tone.Reverb({
+    //     decay: 1,
+    //     preDelay: 0.01
+    // }).toDestination();
+
 
     // play sound
-    let synth = new Tone.Synth().toDestination();
-    synth.triggerAttackRelease(notePitch, '8n', undefined, volume);
+    let synth = new Tone.Synth({
+        oscillator: {
+            type: 'triangle'
+        },
+        envelope: {
+            attack: 0.01,
+            decay: 0.1,
+            sustain: 0.8,
+            release: 0.3
+        }
+    }).toDestination();
+    synth.triggerAttackRelease(notePitch, '32n', undefined, volume);
 
     // Only trigger synth if body is moving fast enough
-    if (volume > .2) {
+    if (volume > .1) {
         console.log("PITCH", pitch, "VOLUME", volume);
         if(body.type === 'circle') {
-            synth.triggerAttackRelease(notePitch, '8n', undefined, volume);
+            synth.triggerAttackRelease(notePitch, '32n', undefined, volume);
             console.log("PITCH", pitch, "VOLUME", volume);
         } else if( body.type === 'rectangle') {
-            synth.triggerAttackRelease(notePitch, '8n', undefined, volume);
+            synth.triggerAttack("notePitch", volume)
             console.log("PITCH", pitch, "VOLUME", volume);
         } else if( body.type === 'polygon') {
-            synth.triggerAttackRelease(notePitch, '8n', undefined, volume);
+            synth.triggerAttackRelease(notePitch, '32n', undefined, volume);
             console.log("PITCH", pitch, "VOLUME", volume);
         } else {
-            synth.triggerAttackRelease(notePitch, '8n', undefined, volume);
+            synth.triggerAttackRelease(notePitch, '32n', undefined, volume);
             console.log("PITCH", pitch, "VOLUME", volume);
         }
-      
+
     }
+
 
     // refresh synth after 1 second (prevents bug where synth stops after a few seconds)
     setInterval(function () {
@@ -143,7 +196,7 @@ Matter.Events.on(engine, 'collisionStart', function (event) {
 
     // change color of body on collision to random color
     body.render.fillStyle = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
-    
+
 
 
 });
@@ -178,7 +231,7 @@ Matter.Events.on(mouseConstraint, 'mousedown', function (event) {
 function startApp() {
     // only allow one click per page load
     document.removeEventListener('click', startApp);
-    Matter.World.add(engine.world, [ground, leftWall, rightWall, ball, sling, stack, mouseConstraint]);
+    Matter.World.add(engine.world, [ground, leftWall, rightWall, leftAngularWall1, rightAngularWall1, leftAngularWall2, rightAngularWall2, stack, mouseConstraint]);
     Matter.Runner.run(engine);
     Matter.Render.run(render);
 }
