8f43a27b61501578db39e48c7f8450a6849ab104
Kenny Johnson <105308787+papercircuit@users.noreply.github.com>
2023-03-12 16:38:19 -0400

    Code formatting applied

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 let engine = Matter.Engine.create();
-
 let render = Matter.Render.create({
     element: document.body,
     engine: engine,
@@ -28,14 +27,9 @@ let rightWall = Matter.Bodies.rectangle(800, 400, 60, 1200, { isStatic: true });
 
 // create angular "walls" to keep ball in play
 let leftAngularWall1 = Matter.Bodies.rectangle(340, 200, 10, 200, { isStatic: true, angle: 2 });
-
 let rightAngularWall1 = Matter.Bodies.rectangle(500, 400, 10, 200, { isStatic: true, angle: -2 });
-
 let leftAngularWall2 = Matter.Bodies.rectangle(300, 600, 10, 200, { isStatic: true, angle: 2 });
-
 let rightAngularWall2 = Matter.Bodies.rectangle(500, 800, 10, 200, { isStatic: true, angle: -2 });
-
-
 let mouse = Matter.Mouse.create(render.canvas);
 let mouseConstraint = Matter.MouseConstraint.create(engine, {
     mouse: mouse,
@@ -45,9 +39,7 @@ let mouseConstraint = Matter.MouseConstraint.create(engine, {
 });
 render.mouse = mouse;
 
-
 // RANDOM BODIES
-
 let stack = Matter.Composites.stack(200, 100, 5, 5, 0, 0, function (x, y) {
     // create random body types and sizes 
     let sides = Math.round(Matter.Common.random(4, 9));
@@ -95,61 +87,19 @@ setInterval(function () {
     }
 }, 1000);
 
-
-
-// BALL AND SLING
-
-// place ball in top center of canvas
-// let ball = Matter.Bodies.circle(600, 50, 25, { restitution: 0.8, lable: 'slingball' });
-// let sling = Matter.Constraint.create({
-//     pointA: { x: 500, y: 0 },
-//     bodyB: ball,
-//     stiffness: 0.005,
-//     length: 100,
-//     render: {
-//         strokeStyle: '#ffffff',
-//         lineWidth: 5
-//     },
-//     label: 'sling'
-// });
-
-
-
-// Create firing event for ball and sling that releases the ball
-let firing = false;
-let fire = function (event) {
-    if (firing) {
-        firing = false;
-        Matter.Body.setPosition(ball, { x: 600, y: 50 });
-        Matter.Body.setVelocity(ball, { x: 0, y: 0 });
-        Matter.Body.setAngularVelocity(ball, 0);
-        Matter.Body.setStatic(ball, true);
-        sling.pointA = { x: 600, y: 150 };
-    }
-    else {
-        firing = true;
-        Matter.Body.setStatic(ball, false);
-        sling.pointA = { x: 600, y: 50 };
-    }
-};
-const randomRadius = () => {
-    return Math.floor(Math.random() * 50) + 10;
-}
-    ;
-
 Matter.Events.on(engine, 'collisionStart', function (event) {
     let body = event.pairs[0].bodyA;
     let velocity = body.velocity;
     let volume = Math.min(Math.abs(velocity.x * .08) + Math.abs(velocity.y * .08), 1);
 
-   
+
     // pitch is determined by the body area
     let pitch = Math.round(body.area / 100) * 100;
 
     // normalize the a multiple of 100
     let note = Math.round(pitch / 100) % 5;
 
-     // quantize pitch to C major pentatonic scale
+    // quantize pitch to C major pentatonic scale
     let notes = [261.63, 329.63, 392, 523.25, 659.25];
 
     // set note pitch to the quantized pitch
@@ -161,7 +111,6 @@ Matter.Events.on(engine, 'collisionStart', function (event) {
         decay: 1,
         preDelay: 0.01
     }).toDestination();
-
 
     // play sound
     let synth = new Tone.Synth({
@@ -182,22 +131,20 @@ Matter.Events.on(engine, 'collisionStart', function (event) {
 
     if (volume > .01 && !synth.playing) {
         console.log("PITCH", pitch, "VOLUME", volume);
-        if(body.type === 'circle') {
+        if (body.type === 'circle') {
             synth.triggerAttackRelease(notePitch, '32n', undefined, volume);
             console.log("PITCH", pitch, "VOLUME", volume);
-        } else if( body.type === 'rectangle') {
+        } else if (body.type === 'rectangle') {
             synth.triggerAttack("notePitch", volume)
             console.log("PITCH", pitch, "VOLUME", volume);
-        } else if( body.type === 'polygon') {
+        } else if (body.type === 'polygon') {
             synth.triggerAttackRelease(notePitch, '32n', undefined, volume);
             console.log("PITCH", pitch, "VOLUME", volume);
         } else {
             synth.triggerAttackRelease(notePitch, '32n', undefined, volume);
             console.log("PITCH", pitch, "VOLUME", volume);
         }
-
     }
-
     // refresh synth and effets (prevents bug where sound stops after a few seconds)
     setInterval(function () {
         synth.dispose();
@@ -206,27 +153,7 @@ Matter.Events.on(engine, 'collisionStart', function (event) {
 
     // change color of body on collision to random color
     body.render.fillStyle = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
-
-
-
 });
-
-// add wind effect to bodies from left to right
-// wind will change speed gradually up and down
-
-
-// Matter.Events.on(engine, 'beforeUpdate', function (event) {
-//     let bodies = Matter.Composite.allBodies(engine.world);
-//     for (let i = 0; i < bodies.length; i++) {
-//         let body = bodies[i];
-//         if (body.position.x < 600) {
-//             Matter.Body.applyForce(body, { x: 0, y: 0 }, { x: wind, y: 0 });
-//         }
-//         else {
-//             Matter.Body.applyForce(body, { x: 0, y: 0 }, { x: .0001, y: 0 });
-//         }
-//     }
-// });
 
 // START APP
 
@@ -236,8 +163,6 @@ Matter.Events.on(mouseConstraint, 'mousedown', function (event) {
     Matter.World.add(engine.world, body);
 });
 
-
-
 function startApp() {
     // only allow one click per page load
     document.removeEventListener('click', startApp);
@@ -246,11 +171,5 @@ function startApp() {
     Matter.Render.run(render);
 }
 
-
 // wait for user interation to start the app
 document.addEventListener('click', startApp);
-
-
-
-
-
